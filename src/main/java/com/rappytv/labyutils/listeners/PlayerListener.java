73eba6bc850c42bb60288c914c139036edee9ef2
RappyTV <rappytv@rappytv.com>
2024-08-17 19:01:47 +0200

    Overhaul PlayerListener with extracted functions and flag cache

--- src/main/java/com/rappytv/labyutils/listeners/PlayerListener.java
+++ src/main/java/com/rappytv/labyutils/listeners/PlayerListener.java
@@ -13,6 +13,7 @@ import net.labymod.serverapi.core.model.moderation.Permission;
 import net.labymod.serverapi.core.model.moderation.RecommendedAddon;
 import net.labymod.serverapi.server.bukkit.LabyModPlayer;
 import net.labymod.serverapi.server.bukkit.event.LabyModPlayerJoinEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -30,6 +31,7 @@ import java.util.function.Consumer;
 
 public class PlayerListener implements Listener {
 
+    private final static Map<UUID, TabListFlag.TabListFlagCountryCode> cachedFlags = new HashMap<>();
     private final static Gson gson = new Gson();
     private final static HttpClient client = HttpClient.newHttpClient();
     private final static String defaultKickMessage = "§c§lKICKED!\n\n§bReason: §7Missing required addons: %s";
@@ -43,133 +45,15 @@ public class PlayerListener implements Listener {
     @EventHandler(ignoreCancelled = true, priority = EventPriority.LOWEST)
     public void onPlayerJoin(LabyModPlayerJoinEvent event) {
         LabyModPlayer labyPlayer = event.labyModPlayer();
-        Player player = labyPlayer.getPlayer();
 
-        if(plugin.getConfig().getBoolean("banner.enabled")) {
-            labyPlayer.sendTabListBanner(plugin.getConfig().getString("banner.url"));
-        }
-
-        if(plugin.getConfig().getBoolean("flags.enabled")) {
-            getCountryCode(player, (flag) -> {
-                if(flag != null) labyPlayer.setTabListFlag(flag);
-            });
-        }
-
-        if(plugin.getConfig().getBoolean("rpc.enabled")) {
-            String text = plugin.getConfig().getString("rpc.text");
-            boolean showTime = plugin.getConfig().getBoolean("rpc.showJoinTime");
-
-            if(text == null) return;
-            if(plugin.isUsingPapi()) {
-                text = PlaceholderAPI.setPlaceholders(player, text);
-            }
-
-            DiscordRPC rpc = showTime
-                    ? DiscordRPC.createWithStart(text, System.currentTimeMillis())
-                    : DiscordRPC.create(text);
-
-            labyPlayer.sendDiscordRPC(rpc);
-        }
-
-        if(plugin.getConfig().getBoolean("addons.disabled.enabled")) {
-            labyPlayer.disableAddons(plugin.getConfig().getStringList("addons.disabled.addons"));
-        }
-
-        if(plugin.getConfig().getBoolean("addons.recommendations.enabled")) {
-            List<RecommendedAddon> addons = new ArrayList<>();
-            ConfigurationSection section = plugin
-                    .getConfig()
-                    .getConfigurationSection("addons.recommendations.addons");
-
-            if(section == null) return;
-
-            for(String key : section.getKeys(false)) {
-                addons.add(RecommendedAddon.of(key, section.getBoolean(key)));
-            }
-
-            labyPlayer.sendAddonRecommendations(addons, response -> {
-                if(response.isInitial()) return;
-                if(!response.isAllInstalled()) {
-                    player.kickPlayer(String.format(
-                            section.getString("kickMessage", defaultKickMessage),
-                            String.join(", ", response.getMissingAddons())
-                    ));
-                }
-            });
-        }
-
-        if(plugin.getConfig().getBoolean("subtitles.enabled")) {
-            ConfigurationSection section = plugin
-                    .getConfig()
-                    .getConfigurationSection("subtitles.subtitles");
-
-            if(section == null) return;
-            ServerAPIComponent component = null;
-            double size = -1;
-
-            for(String key : section.getKeys(false)) {
-                String permission = section.getString(key + ".permission");
-                String text = section.getString(key + ".text");
-                if(permission != null && text != null && player.hasPermission(permission)) {
-                    if(plugin.isUsingPapi()) {
-                        text = PlaceholderAPI.setPlaceholders(player, text);
-                    }
-                    component = ServerAPIComponent.text(text);
-                    size = section.getDouble(key + ".size");
-                    break;
-                }
-            }
-            if(component == null || size <= 0) {
-                labyPlayer.resetSubtitle();
-                return;
-            }
-
-            labyPlayer.updateSubtitle(component, size);
-        }
-
-        // TODO: Find error source
-        if(plugin.getConfig().getBoolean("permissions.enabled")) {
-            List<Permission.StatedPermission> permissions = new ArrayList<>();
-            ConfigurationSection section = plugin
-                    .getConfig()
-                    .getConfigurationSection("permissions.permissions");
-
-            if(section == null) return;
-
-            for(String key : section.getKeys(false)) {
-                permissions.add(section.getBoolean(key) ? Permission.of(key).allow() : Permission.of(key).deny());
-            }
-
-            labyPlayer.sendPermissions(permissions);
-        }
-
-        if(plugin.getConfig().getBoolean("interactions.enabled")) {
-            List<InteractionMenuEntry> entries = new ArrayList<>();
-
-            // TODO: Rewrite
-            List<InteractionBullet> bullets = (List<InteractionBullet>) plugin.getConfig().getList("interactions.bullets", Collections.emptyList());
-            for(InteractionBullet bullet : bullets) {
-                if(bullet.permission != null && !player.hasPermission(bullet.permission)) continue;
-                if(bullet.title == null || bullet.title.isBlank() || bullet.type == null || bullet.value == null) continue;
-                InteractionMenuEntry.InteractionMenuType type;
-                try {
-                    type = InteractionMenuEntry.InteractionMenuType.valueOf(
-                            bullet.type
-                                    .toUpperCase()
-                                    .replace(' ', '_')
-                    );
-                } catch (Exception e) {
-                    continue;
-                }
-                entries.add(InteractionMenuEntry.create(
-                        ServerAPIComponent.text(bullet.title),
-                        type,
-                        bullet.value
-                ));
-            }
-
-            if(!entries.isEmpty()) labyPlayer.sendInteractionMenuEntries(entries);
-        }
+        setBanner(labyPlayer);
+        setFlag(labyPlayer);
+        setSubtitle(labyPlayer);
+        setInteractionBullets(labyPlayer);
+        disableAddons(labyPlayer);
+        recommendAddons(labyPlayer);
+        managePermissions(labyPlayer);
+        setRPC(labyPlayer);
     }
 
     @EventHandler
@@ -178,7 +62,142 @@ public class PlayerListener implements Listener {
         EconomyBalanceUpdateEvent.bankBalances.remove(event.getPlayer().getUniqueId());
     }
 
-    public void getCountryCode(Player player, Consumer<TabListFlag.@Nullable TabListFlagCountryCode> consumer) {
+    private void setBanner(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("banner.enabled")) return;
+        player.sendTabListBanner(plugin.getConfig().getString("banner.url"));
+    }
+
+    private void setFlag(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("flags.enabled")) return;
+        getCountryCode(player.getPlayer(), (flag) -> {
+            if(flag != null) Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, () -> player.setTabListFlag(flag));
+        });
+    }
+
+    private void setSubtitle(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("subtitles.enabled")) return;
+        ConfigurationSection section = plugin
+                .getConfig()
+                .getConfigurationSection("subtitles.subtitles");
+
+        if(section == null) return;
+        ServerAPIComponent component = null;
+        double size = -1;
+
+        for(String key : section.getKeys(false)) {
+            String permission = section.getString(key + ".permission");
+            String text = section.getString(key + ".text");
+            if(permission != null && text != null && player.getPlayer().hasPermission(permission)) {
+                if(plugin.isUsingPapi()) {
+                    text = PlaceholderAPI.setPlaceholders(player.getPlayer(), text);
+                }
+                component = ServerAPIComponent.text(text);
+                size = section.getDouble(key + ".size");
+                break;
+            }
+        }
+        if(component == null || size <= 0) {
+            player.resetSubtitle();
+            return;
+        }
+
+        player.updateSubtitle(component, size);
+    }
+
+    private void setInteractionBullets(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("interactions.enabled")) return;
+        List<InteractionMenuEntry> entries = new ArrayList<>();
+        ConfigurationSection section = plugin
+                .getConfig()
+                .getConfigurationSection("interactions.bullets");
+
+        for(String key : section.getKeys(false)) {
+            if(section.isString(key + ".permission")
+                    && !player.getPlayer().hasPermission(section.getString(key + ".permission"))) continue;
+            try {
+                entries.add((InteractionMenuEntry.create(
+                        ServerAPIComponent.text(section.getString(key + ".title")),
+                        InteractionMenuEntry.InteractionMenuType.valueOf(
+                                section.getString(key + ".type").toUpperCase().replace(' ', '_')
+                        ),
+                        section.getString(key + ".value")
+                )));
+            } catch (Exception e) {
+                plugin.getLogger().warning("Failed to build interaction bullet with id " + key);
+            }
+        }
+
+        if(!entries.isEmpty()) player.sendInteractionMenuEntries(entries);
+    }
+
+    private void disableAddons(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("addons.disabled.enabled")) return;
+        player.disableAddons(plugin.getConfig().getStringList("addons.disabled.addons"));
+    }
+
+    private void recommendAddons(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("addons.recommendations.enabled")) return;
+        List<RecommendedAddon> addons = new ArrayList<>();
+        ConfigurationSection section = plugin
+                .getConfig()
+                .getConfigurationSection("addons.recommendations.addons");
+
+        if(section == null) return;
+
+        for(String key : section.getKeys(false)) {
+            addons.add(RecommendedAddon.of(key, section.getBoolean(key)));
+        }
+
+        player.sendAddonRecommendations(addons, response -> {
+            if(response.isInitial()) return;
+            if(!response.isAllInstalled()) {
+                if(player.getPlayer().isOnline()) player.getPlayer().kickPlayer(String.format(
+                        section.getString("kickMessage", defaultKickMessage),
+                        String.join(", ", response.getMissingAddons())
+                ));
+            }
+        });
+    }
+
+    private void managePermissions(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("permissions.enabled")) return;
+        // TODO: Find error source
+        List<Permission.StatedPermission> permissions = new ArrayList<>();
+        ConfigurationSection section = plugin
+                .getConfig()
+                .getConfigurationSection("permissions.permissions");
+
+        if(section == null) return;
+
+        for(String key : section.getKeys(false)) {
+            permissions.add(section.getBoolean(key) ? Permission.of(key).allow() : Permission.of(key).deny());
+        }
+
+        player.sendPermissions(permissions);
+    }
+
+    private void setRPC(LabyModPlayer player) {
+        if(!plugin.getConfig().getBoolean("rpc.enabled")) return;
+        String text = plugin.getConfig().getString("rpc.text");
+        boolean showTime = plugin.getConfig().getBoolean("rpc.showJoinTime");
+
+        if(text == null) return;
+        if(plugin.isUsingPapi()) {
+            text = PlaceholderAPI.setPlaceholders(player.getPlayer(), text);
+        }
+
+        DiscordRPC rpc = showTime
+                ? DiscordRPC.createWithStart(text, System.currentTimeMillis())
+                : DiscordRPC.create(text);
+
+        player.sendDiscordRPC(rpc);
+    }
+
+    private void getCountryCode(Player player, Consumer<TabListFlag.@Nullable TabListFlagCountryCode> consumer) {
+        if(cachedFlags.containsKey(player.getUniqueId())) {
+            consumer.accept(cachedFlags.get(player.getUniqueId()));
+            return;
+        }
         if(player.getAddress() == null) {
             consumer.accept(null);
             return;
@@ -192,7 +211,8 @@ public class PlayerListener implements Listener {
 
             client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).thenAccept(response -> {
                 JsonObject body = gson.fromJson(response.body(), JsonObject.class);
-                consumer.accept(TabListFlag.TabListFlagCountryCode.valueOf(body.get("country").getAsString()));
+                cachedFlags.put(player.getUniqueId(), TabListFlag.TabListFlagCountryCode.valueOf(body.get("country").getAsString()));
+                consumer.accept(cachedFlags.get(player.getUniqueId()));
             }).exceptionally(throwable -> {
                 plugin.getLogger().warning("Failed to get country code of " + host);
                 consumer.accept(null);
@@ -202,12 +222,5 @@ public class PlayerListener implements Listener {
             plugin.getLogger().warning("Failed to get country code of " + host);
             consumer.accept(null);
         }
-    }
-
-    private static class InteractionBullet {
-        public String title;
-        public String permission;
-        public String type;
-        public String value;
     }
 }
