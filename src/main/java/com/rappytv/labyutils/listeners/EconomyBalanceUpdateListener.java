495db3475bf49cc0a7e2ddb17efda06459929495
RappyTV <rappytv@rappytv.com>
2024-08-17 04:51:09 +0200

    Listen to EconomyBalanceUpdateEvent

---
+++ src/main/java/com/rappytv/labyutils/listeners/EconomyBalanceUpdateListener.java
@@ -0,0 +1,27 @@
+package com.rappytv.labyutils.listeners;
+
+import com.rappytv.labyutils.LabyUtilsPlugin;
+import com.rappytv.labyutils.events.EconomyBalanceUpdateEvent;
+import net.labymod.serverapi.server.bukkit.LabyModPlayer;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+
+public class EconomyBalanceUpdateListener implements Listener {
+
+    private final LabyUtilsPlugin plugin;
+
+    public EconomyBalanceUpdateListener(LabyUtilsPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    @EventHandler
+    public void onBalanceUpdate(EconomyBalanceUpdateEvent event) {
+        if(plugin.getEconomy() == null) return;
+        LabyModPlayer player = event.getPlayer();
+
+        switch (event.getBalanceType()) {
+            case CASH -> player.updateCashEconomy(economy -> economy.balance(event.getBalance()));
+            case BANK -> player.updateBankEconomy(economy -> economy.balance(event.getBalance()));
+        }
+    }
+}
