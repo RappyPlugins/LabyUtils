d3e1c00a41cc45a5587723c425ac33192fab173d
RappyTV <rappytv@rappytv.com>
2024-08-29 02:55:45 +0200

    Save default config if doesn't exist

--- src/main/java/com/rappytv/labyutils/bungee/LabyUtilsConfig.java
+++ src/main/java/com/rappytv/labyutils/bungee/LabyUtilsConfig.java
@@ -5,36 +5,41 @@ import net.md_5.bungee.config.ConfigurationProvider;
 import net.md_5.bungee.config.YamlConfiguration;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.logging.Logger;
+import java.io.InputStream;
 
 public class LabyUtilsConfig {
 
     private static final ConfigurationProvider provider = ConfigurationProvider.getProvider(YamlConfiguration.class);
-    private final Logger logger;
-    private File configFolder;
-    private File configFile;
+    private final LabyUtilsBungee plugin;
+    private final File configFolder;
+    private final File configFile;
     private Configuration configuration;
 
     public LabyUtilsConfig(LabyUtilsBungee plugin) {
-        logger = plugin.getLogger();
+        this.plugin = plugin;
         configFolder = plugin.getDataFolder();
         configFile = new File(configFolder, "config.yml");
         reload();
     }
 
+    @SuppressWarnings("ResultOfMethodCallIgnored")
     public void reload() {
         try {
             if(!configFolder.exists()) {
                 configFolder.mkdir();
             }
             if(!configFile.exists()) {
-                configFile.createNewFile();
+                FileOutputStream outputStream = new FileOutputStream(configFile);
+                InputStream in = plugin.getResourceAsStream("config.yml");
+                in.transferTo(outputStream);
+                in.close();
             }
 
             configuration = provider.load(configFile);
         } catch (IOException e) {
-            logger.severe("Failed to load plugin configuration!");
+            plugin.getLogger().severe("Failed to load plugin configuration!");
         }
     }
 
