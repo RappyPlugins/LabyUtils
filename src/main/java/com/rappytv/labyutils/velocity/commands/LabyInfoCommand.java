2925467076936121533f179aad606f2d3170a9b5
RappyTV <rappytv@rappytv.com>
2024-09-13 00:34:55 +0200

    Fix error message when no player argument was passed

--- src/main/java/com/rappytv/labyutils/velocity/commands/LabyInfoCommand.java
+++ src/main/java/com/rappytv/labyutils/velocity/commands/LabyInfoCommand.java
@@ -8,6 +8,7 @@ import com.rappytv.labyutils.velocity.LabyUtilsVelocity;
 import com.velocitypowered.api.command.BrigadierCommand;
 import com.velocitypowered.api.command.CommandSource;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.ComponentSerializer;
 import net.labymod.serverapi.api.model.component.ServerAPITextComponent;
 import net.labymod.serverapi.server.velocity.LabyModPlayer;
 import net.labymod.serverapi.server.velocity.LabyModProtocolService;
@@ -20,7 +21,7 @@ public class LabyInfoCommand {
                 .executes(context -> {
                     plugin.getConfigManager().reloadConfig();
                     context.getSource().sendMessage(Component.text(
-                            LabyUtilsVelocity.getPrefix() + "§7Addon config successfully reloaded!"
+                            LabyUtilsVelocity.getPrefix() + "§cPlease enter a player name!"
                     ));
 
                     return Command.SINGLE_SUCCESS;
